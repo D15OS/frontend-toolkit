4d541723d68b788f8f0df6220a36e509adb3940a
DR <1157185904@qq.com>
2023-06-12 18:15:05 +0800

    add

--- main/src/views/HomeView.vue
+++ main/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
   <main>
-    <TheWelcome />
+    <WujieVue width="100%" height="100%" name="xxx" :url="url" ></WujieVue>
   </main>
 </template>
+<script setup lang='ts'>
+const url = 'http://127.0.0.1:5174/'
+</script>
